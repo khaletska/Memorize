37498dcbfb71e28e7c3914f1aea7365b7f23aa85
Elena Khaletska <yeliena.khaletska@pipedrive.com>
2024-06-30 11:03:00 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Yeliena Khaletska on 30.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
